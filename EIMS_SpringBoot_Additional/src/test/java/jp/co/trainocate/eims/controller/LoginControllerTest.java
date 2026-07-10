047654181350c3bbff75543bd8344f68857ca12f
Juyeong-Jung <nurinubi.jung.juyeong@gmail.com>
2026-07-10 14:23:43 +0900

    追加機能設計書とログインテストを修正

--- EIMS_SpringBoot_Additional/src/test/java/jp/co/trainocate/eims/controller/LoginControllerTest.java
+++ EIMS_SpringBoot_Additional/src/test/java/jp/co/trainocate/eims/controller/LoginControllerTest.java
@@ -53,6 +53,76 @@ class LoginControllerTest {
                 .andExpect(model().attributeHasFieldErrors("loginForm", "password"));
     }
 
+    @Test
+    @DisplayName("未入力の場合は入力エラーになる")
+    void loginRejectsEmptyInput() throws Exception {
+        mockMvc.perform(post("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeHasFieldErrors("loginForm", "empNo", "password"));
+    }
+
+    @Test
+    @DisplayName("3文字のパスワードは入力エラーになる")
+    void loginRejectsThreeCharacterPassword() throws Exception {
+        mockMvc.perform(post("/login")
+                .param("empNo", "10003")
+                .param("password", "abc"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attributeHasFieldErrors("loginForm", "password"));
+    }
+
+    @Test
+    @DisplayName("存在しない社員番号ではログインできない")
+    void loginRejectsUnknownEmployee() throws Exception {
+        when(employeeService.findById(99999)).thenReturn(null);
+
+        mockMvc.perform(post("/login")
+                .param("empNo", "99999")
+                .param("password", "password"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("loginError", true));
+    }
+
+    @Test
+    @DisplayName("パスワードが一致しない場合はログインできない")
+    void loginRejectsMismatchedPassword() throws Exception {
+        when(employeeService.findById(10003)).thenReturn(employee("password"));
+
+        mockMvc.perform(post("/login")
+                .param("empNo", "10003")
+                .param("password", "different"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("loginError", true));
+    }
+
+    @Test
+    @DisplayName("退職済み社員はログインできない")
+    void loginRejectsRetiredEmployee() throws Exception {
+        Employee retiredEmployee = employee("password");
+        retiredEmployee.setDeleteFlg(1);
+        when(employeeService.findById(10003)).thenReturn(retiredEmployee);
+
+        mockMvc.perform(post("/login")
+                .param("empNo", "10003")
+                .param("password", "password"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("loginError", true));
+    }
+
+    @Test
+    @DisplayName("ログアウトするとセッションを破棄してログイン画面へ戻る")
+    void logoutInvalidatesSession() throws Exception {
+        mockMvc.perform(post("/logout").sessionAttr("loginEmployee", employee("password")))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"))
+                .andExpect(request().sessionAttributeDoesNotExist("loginEmployee"));
+    }
+
     private Employee employee(String password) {
         return new Employee(10003, "山田", "太郎", "ヤマダ", "タロウ",
                 password, 1, new Department(100, "人事部"), 0, 0);
